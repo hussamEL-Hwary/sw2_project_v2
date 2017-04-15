ad511c2b7bcd248e8597a2da646b83632b7afb16
hussamEL-Hwary <hossam.elhwary1996@gmail.com>
2017-04-15 16:23:08 +0200

    student class

--- gameappl/src/main/java/gameappl/domain/Student.java
+++ gameappl/src/main/java/gameappl/domain/Student.java
@@ -1,23 +1,44 @@
 package gameappl.domain;
 
 
+import java.util.Set;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import gameappl.domain.Date;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+
 @Entity
+@Table(name="student")
 public class Student {
-    @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
+    
     private int id;
 	private String name;
 	private String password;
 	private int age;
 	private String mail;
 	private String gender;
+	private Set<ScoreDetail>myScores;
 	
+	public Student(int id, String name, String password, int age) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.password = password;
+		this.age = age;
+		
+	}
+	public Student() {
+		super();
+		
+	}
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	public int getId() {
 		return id;
 	}
@@ -42,20 +63,43 @@ public class Student {
 	public void setAge(int age) {
 		this.age = age;
 	}
-	
-	public Student(int id, String name, String password, int age) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.password = password;
-		this.age = age;
-		
+	public String getMail() {
+		return mail;
 	}
-	public Student() {
-		super();
-		// TODO Auto-generated constructor stub
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	/*
+	@OneToMany(mappedBy = "Student", cascade = CascadeType.ALL)
+	public Set<ScoreDetail> getmyScore()
+	{
+		return myScores;
 	}
 	
+	public void setmyScore(Set<ScoreDetail> scores)
+	{
+		this.myScores=scores;
+	}
 	
+	/*@Override
+	public String toString(){
+		String result=String.format("Student[id=%d,name='%s',passwors='%s',age='%s',mail='%s',gender='%s']",
+				id,name,password,age,mail,gender);
+		if(myScores!=null)
+		{
+			for(ScoreDetail score:myScores)
+			{
+				result+=String.format("ScoreDetail[id=%d,name='%s',score=%d]",score.getId(),score.getGameName(),score.getScore() );
+			}
+		}
+		return result;
+	}
+	*/
 	
 }
