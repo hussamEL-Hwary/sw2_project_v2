dad9d1c82cc3a23b5a45b9ad19762af7283b76aa
hussamEL-Hwary <hossam.elhwary1996@gmail.com>
2017-04-15 18:39:06 +0200

    add teacher class

---
+++ gameappl/src/main/java/gameappl/domain/Teacher.java
@@ -0,0 +1,48 @@
+package gameappl.domain;
+
+public class Teacher {
+
+	private int id;
+	private String name;
+	private String password;
+	private int age;
+	private String mail;
+	private String gender;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getMail() {
+		return mail;
+	}
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	
+}
