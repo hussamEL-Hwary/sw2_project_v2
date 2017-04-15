b0d674023502cf2f4210b2082095849b0cad5570
hussamEL-Hwary <hossam.elhwary1996@gmail.com>
2017-04-15 16:21:37 +0200

    add student controller class

--- gameappl/src/main/java/gameappl/controller/StudentController.java
+++ gameappl/src/main/java/gameappl/controller/StudentController.java
@@ -3,11 +3,14 @@ package gameappl.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import gameappl.domain.ScoreDetail;
 import gameappl.domain.Student;
+import gameappl.repo.ScoreDetailRepo;
 import gameappl.repo.StudentRepo;
 
 @RestController
@@ -16,12 +19,21 @@ public class StudentController {
 
 	@Autowired
 	
-	StudentRepo rp;
-
-	@RequestMapping("/findall")
-    @ResponseBody
-	public List<Student> findall(){
-	return rp.findAll();
+	private StudentRepo studentRepo;
+	@Autowired
+	private ScoreDetailRepo sRepo;
+	
+	
+	@GetMapping(path="/all")
+	public @ResponseBody List<Student> findall(){
+	return studentRepo.findAll();
 	}
+	
+	@GetMapping(path="/thi")
+	public @ResponseBody List<ScoreDetail>ff()
+	{
+		return sRepo.findAll();
+	}
+	
 	
 }
