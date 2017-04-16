ec74a6d0c18976301c95d4d098540b895982292f
hussamEL-Hwary <hossam.elhwary1996@gmail.com>
2017-04-16 15:46:30 +0200

    make class entity

--- gameappl/src/main/java/gameappl/domain/Category.java
+++ gameappl/src/main/java/gameappl/domain/Category.java
@@ -1,10 +1,14 @@
 package gameappl.domain;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name="categories")
 public class Category {
 
 	@Id
@@ -23,6 +27,23 @@ public class Category {
 		super();
 		// TODO Auto-generated constructor stub
 	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
 	
 	
 }
