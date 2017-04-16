848541bc0d849530221038b8509e29c03e88a8c2
hussamEL-Hwary <hossam.elhwary1996@gmail.com>
2017-04-16 15:39:48 +0200

    separate student functions in controller

--- gameappl/src/main/java/gameappl/controller/StudentController.java
+++ gameappl/src/main/java/gameappl/controller/StudentController.java
@@ -3,8 +3,10 @@ package gameappl.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,15 +15,13 @@ import gameappl.domain.Student;
 import gameappl.repo.ScoreDetailRepo;
 import gameappl.repo.StudentRepo;
 
-@RestController
+@Controller
 @RequestMapping("/student")
 public class StudentController {
 
 	@Autowired
 	
 	private StudentRepo studentRepo;
-	@Autowired
-	private ScoreDetailRepo sRepo;
 	
 	
 	@GetMapping(path="/all")
@@ -29,11 +29,27 @@ public class StudentController {
 	return studentRepo.findAll();
 	}
 	
-	@GetMapping(path="/thi")
-	public @ResponseBody List<ScoreDetail>ff()
-	{
-		return sRepo.findAll();
+	
+	
+	@RequestMapping("/signup_student")
+	public String signup_student(){
+		return "signup_student";
 	}
 	
+	//this service function to add new student in database
+		@GetMapping("/add_st_DB")
+		public String add_st_DB(
+				@RequestParam("name") String name,
+				@RequestParam("email") String email,
+				@RequestParam("password") String password,
+				@RequestParam("age") int age,
+				@RequestParam("gender") String gender
+				){
+			//create new student to save in student database
+			Student studentToSave = new Student(name,password,age,email,gender); 
+			studentRepo.save(studentToSave);
+			//return the generalView page to user
+			return "generalView";
+		}
 	
 }
