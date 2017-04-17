6dd0b8513d872a51a8ad1ef7ca1bf3f4ab0b0c2b
hussamEL-Hwary <hossam.elhwary1996@gmail.com>
2017-04-17 17:19:19 +0200

    add Game Entity class

---
+++ gameappl/src/main/java/gameappl/domain/Game.java
@@ -0,0 +1,84 @@
+package gameappl.domain;
+
+import java.util.ArrayList;
+
+import javax.persistence.CollectionTable;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Inheritance
+public abstract class Game {
+
+	@Id
+	private int id;
+	
+	@Column(unique=true) 
+	@NotNull
+	private String name;
+	 
+	@NotNull
+	private String creatorName;
+	
+	@NotNull
+	private String type;
+	
+	private double rate;
+	
+	private ArrayList<String> comments=new ArrayList<String>();
+
+	
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCreatorName() {
+		return creatorName;
+	}
+
+	public void setCreatorName(String creatorName) {
+		this.creatorName = creatorName;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public double getRate() {
+		return rate;
+	}
+
+	public void setRate(double rate) {
+		this.rate = rate;
+	}
+
+	public ArrayList<String> getComments() {
+		return comments;
+	}
+
+	public void setComments(ArrayList<String> comments) {
+		this.comments = comments;
+	}
+
+	
+	
+	
+
+	
+	
+	
+}
