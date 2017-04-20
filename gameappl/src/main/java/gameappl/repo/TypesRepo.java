a8524323c77c8e68471e6c38b8f9270bca57b45a
hussamEL-Hwary <hossam.elhwary1996@gmail.com>
2017-04-20 15:51:05 +0200

    add game type repo class

---
+++ gameappl/src/main/java/gameappl/repo/TypesRepo.java
@@ -0,0 +1,9 @@
+package gameappl.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import gameappl.domain.Types;
+
+public interface TypesRepo extends JpaRepository<Types,Long> {
+
+}
