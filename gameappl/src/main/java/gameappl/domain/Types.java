30c2db6075babe50c1e0135733662009e5b2731f
hussamEL-Hwary <hossam.elhwary1996@gmail.com>
2017-04-20 20:06:44 +0200

    types class comments

--- gameappl/src/main/java/gameappl/domain/Types.java
+++ gameappl/src/main/java/gameappl/domain/Types.java
@@ -4,34 +4,63 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
+/**
+ * Entity type class represent game type
+ * @author Hussam
+ *
+ */
 @Entity
 public class Types {
-	
-	  
+	//==============
+	//PRIVATE FIELDS
+	//==============  
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
    private int id;
    private String name;
 	
-	
-   
+   //==============
+   //PUBLIC METHODS
+   //==============
+   /**
+    * Default constructor
+    */
 	public Types() {
 	super();
 	}
+	/**
+	 * parameterize constructor 
+	 * @param name
+	 */
 	public Types(String name) {
 	super();
 	this.name = name;
     }
+	/**
+	 * 
+	 * @return type id
+	 */
 	public int getId() {
 		return id;
 	}
+	/**
+	 * 
+	 * @param id The id to set
+	 */
 	public void setId(int id) {
 		this.id = id;
 	}
+	/**
+	 * 
+	 * @return type name
+	 */
 	public String getName() {
 		return name;
 	}
+	/**
+	 * 
+	 * @param name the name to set
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
