0d95b117f6884da40b46a88529e3134de9b4df7d
hussamEL-Hwary <hossam.elhwary1996@gmail.com>
2017-04-20 15:59:13 +0200

    add comments to be readable

--- gameappl/src/main/java/gameappl/Application.java
+++ gameappl/src/main/java/gameappl/Application.java
@@ -2,10 +2,19 @@ package gameappl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**
+ * main class of application
+ * system run  
+ * @author Hussam
+ *
+ */
 @SpringBootApplication
 public class Application {
 
+	/**
+	 * main function system start run from
+	 * @param args
+	 */
 	public static void main(String[] args) {
 
     SpringApplication.run(Application.class, args);
