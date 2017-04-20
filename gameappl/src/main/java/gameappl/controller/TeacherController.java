1394e9df00e6e400d391e85bddf652cd0f583374
hussamEL-Hwary <hossam.elhwary1996@gmail.com>
2017-04-20 16:32:43 +0200

    Teacher Controller comments

--- gameappl/src/main/java/gameappl/controller/TeacherController.java
+++ gameappl/src/main/java/gameappl/controller/TeacherController.java
@@ -8,39 +8,46 @@ import org.springframework.web.bind.annotation.RequestParam;
 import gameappl.domain.Teacher;
 import gameappl.repo.TeacherRepo;
 
-/*
+/**
  * teacher controller class 
- * make services function to help teacher to 
- * sign up ,
+ * make services function  
+ * @author Hussam
  * */
 @Controller
 @RequestMapping("/teacher")
 public class TeacherController {
-	
-	//to execute teacher query
+
+	// to execute teacher query
 	@Autowired
-	private TeacherRepo teacherRepo; 
-	
-	//this function return the form view to user
+	private TeacherRepo teacherRepo;
+
+	/**
+	 * 
+	 * @return form view to user
+	 */
 	@RequestMapping("/signUpForm")
-	public String SignUpForm()
-	{
+	public String SignUpForm() {
 		return "signup_teacher";
 	}
-
-	@RequestMapping("/addTeacher")
-	public String addTeacherToDB(
-			@RequestParam("name") String name,
-			@RequestParam("email") String email,
-			@RequestParam("password") String password,
-			@RequestParam("age") int age,
-			@RequestParam("gender") String gender
-			){
-		Teacher teacherToSave=new Teacher(name,password,age,email,gender);
-		teacherRepo.save(teacherToSave);
-		
-		return "generalView";
-		
-	}
 	
-}
+    /**
+     * add teacher in db
+     * @param name
+     * @param email
+     * @param password
+     * @param age
+     * @param gender
+     * @return generalView for teacher to log in
+     */
+	@RequestMapping("/addTeacher")
+	public String addTeacherToDB(@RequestParam("name") String name, @RequestParam("email") String email,
+			@RequestParam("password") String password, @RequestParam("age") int age,
+			@RequestParam("gender") String gender) {
+		Teacher teacherToSave = new Teacher(name, password, age, email, gender);
+		teacherRepo.save(teacherToSave);
+
+		return "generalView";
+
+	}
+
+}//class TeacherController
