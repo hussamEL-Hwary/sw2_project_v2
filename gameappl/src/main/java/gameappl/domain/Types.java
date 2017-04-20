afc1af92c42f21d28295ab79dfd315fa11781854
hussamEL-Hwary <hossam.elhwary1996@gmail.com>
2017-04-20 15:50:22 +0200

    add game Types class to help us find the types of games

---
+++ gameappl/src/main/java/gameappl/domain/Types.java
@@ -0,0 +1,39 @@
+package gameappl.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Types {
+	
+	  
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+   private int id;
+   private String name;
+	
+	
+   
+	public Types() {
+	super();
+	}
+	public Types(String name) {
+	super();
+	this.name = name;
+    }
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
