6bafffe9b87cdc731bd4687c89141d55d001e036
hussamEL-Hwary <hossam.elhwary1996@gmail.com>
2017-04-20 17:18:37 +0200

    teacher comments

--- gameappl/src/main/java/gameappl/domain/Teacher.java
+++ gameappl/src/main/java/gameappl/domain/Teacher.java
@@ -6,10 +6,19 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+/**
+ * Entity teacher class to represent teacher in db
+ * @author Hussam
+ *
+ */
 @Entity
 @Table(name="teacher")
 public class Teacher {
     
+	//==============
+	//PRIVATE FIELDS
+	//==============
+	
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	private int id;
@@ -19,6 +28,19 @@ public class Teacher {
 	private String mail;
 	private String gender;
 	
+	//==============
+	//PUBLIC METHODS
+	//==============
+	
+	
+	/**
+	 * Parameterize constructor
+	 * @param name
+	 * @param password
+	 * @param age
+	 * @param mail
+	 * @param gender
+	 */
 	public Teacher(String name, String password, int age, String mail, String gender) {
 		super();
 		this.name = name;
@@ -28,48 +50,97 @@ public class Teacher {
 		this.gender = gender;
 	}
 	
-	
+	/**
+	 * Default constructor
+	 */
 	public Teacher() {
 		super();
 		
 	}
 
-
+	/**
+	 * 
+	 * @return teacher id
+	 */
 	public int getId() {
 		return id;
 	}
+	/**
+	 * 
+	 * @param id The id to set
+ 	 */
 	public void setId(int id) {
 		this.id = id;
 	}
+	/**
+	 * 
+	 * @return teacher name
+	 */
 	public String getName() {
 		return name;
 	}
+	/**
+	 * 
+	 * @param name The name to set
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
+	/**
+	 * 
+	 * @return teacher password
+	 */
 	public String getPassword() {
 		return password;
 	}
+	/**
+	 * 
+	 * @param password The password to set
+	 */
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	/**
+	 * 
+	 * @return teacher age
+	 */
 	public int getAge() {
 		return age;
 	}
+	/**
+	 * 
+	 * @param age The age to set
+	 */
 	public void setAge(int age) {
 		this.age = age;
 	}
+	/**
+	 * 
+	 * @return teacher mail
+	 */
 	public String getMail() {
 		return mail;
 	}
+	/**
+	 * 
+	 * @param mail The mail to set
+	 */
 	public void setMail(String mail) {
 		this.mail = mail;
 	}
+	/**
+	 * 
+	 * @return teacher gender
+	 */
 	public String getGender() {
 		return gender;
 	}
+	/**
+	 * 
+	 * @param gender The gender to set
+	 */
 	public void setGender(String gender) {
 		this.gender = gender;
 	}
 	
-}
+}// class Teacher
