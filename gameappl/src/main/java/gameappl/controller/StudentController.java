d67bf556f834d7edf0d652d2b6daea94da534fce
hussamEL-Hwary <hossam.elhwary1996@gmail.com>
2017-04-20 16:27:18 +0200

    StudentControoler comments

--- gameappl/src/main/java/gameappl/controller/StudentController.java
+++ gameappl/src/main/java/gameappl/controller/StudentController.java
@@ -15,41 +15,57 @@ import gameappl.domain.Student;
 import gameappl.repo.ScoreDetailRepo;
 import gameappl.repo.StudentRepo;
 
+/**
+ * Controller class for mapping student services functions
+ * 
+ * @author Hussam
+ *
+ */
 @Controller
 @RequestMapping("/student")
 public class StudentController {
 
 	@Autowired
-	
 	private StudentRepo studentRepo;
-	
-	
-	@GetMapping(path="/all")
-	public @ResponseBody List<Student> findall(){
-	return studentRepo.findAll();
+
+	/**
+	 * find all student
+	 * 
+	 * @return Json of student
+	 */
+	@GetMapping(path = "/all")
+	public @ResponseBody List<Student> findall() {
+		return studentRepo.findAll();
 	}
-	
-	
-	
+
+	/**
+	 * @return signup teacher view
+	 */
 	@RequestMapping("/signup_student")
-	public String signup_student(){
+	public String signup_student() {
 		return "signup_student";
 	}
-	
-	//this service function to add new student in database
-		@GetMapping("/add_st_DB")
-		public String add_st_DB(
-				@RequestParam("name") String name,
-				@RequestParam("email") String email,
-				@RequestParam("password") String password,
-				@RequestParam("age") int age,
-				@RequestParam("gender") String gender
-				){
-			//create new student to save in student database
-			Student studentToSave = new Student(name,password,age,email,gender); 
-			studentRepo.save(studentToSave);
-			//return the generalView page to user
-			return "generalView";
-		}
-	
-}
+
+	/**
+	 * add new student in database
+	 * 
+	 * @param name
+	 * @param email
+	 * @param password
+	 * @param age
+	 * @param gender
+	 * @return generalView for student to log in
+	 */
+
+	@GetMapping("/add_st_DB")
+	public String add_st_DB(@RequestParam("name") String name, @RequestParam("email") String email,
+			@RequestParam("password") String password, @RequestParam("age") int age,
+			@RequestParam("gender") String gender) {
+		// create new student to save in student database
+		Student studentToSave = new Student(name, password, age, email, gender);
+		studentRepo.save(studentToSave);
+		// return the generalView page to user
+		return "generalView";
+	}
+
+}// class StudentController
