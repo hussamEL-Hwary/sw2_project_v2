0cea5c96966b07601b8c970b0ff675d4f2c357aa
hussamEL-Hwary <hossam.elhwary1996@gmail.com>
2017-04-29 23:25:04 +0200

    find student by mail

--- gameappl/src/main/java/gameappl/repo/StudentRepo.java
+++ gameappl/src/main/java/gameappl/repo/StudentRepo.java
@@ -1,6 +1,8 @@
 package gameappl.repo;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
+
 
 import gameappl.domain.Student;
 
@@ -10,7 +12,7 @@ import gameappl.domain.Student;
  *
  */
 public interface StudentRepo extends JpaRepository<Student, Long> {
-
+	
 public Student findByMailAndPassword(String mail,String password);
 public Student findByMail(String mail);
 
