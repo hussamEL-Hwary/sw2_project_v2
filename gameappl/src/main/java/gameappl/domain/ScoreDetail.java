aefe7b71a13ed7a1fadd5659471e3fc9187777cb
hussamEL-Hwary <hossam.elhwary1996@gmail.com>
2017-04-29 23:21:19 +0200

    make score detail as normal class

--- gameappl/src/main/java/gameappl/domain/ScoreDetail.java
+++ gameappl/src/main/java/gameappl/domain/ScoreDetail.java
@@ -1,36 +1,22 @@
 package gameappl.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 
-@Entity
 public class ScoreDetail {
 
-	private int id;
+
 	private String gameName;
 	private int score;
-	private Student student;
+	
 	
 	public ScoreDetail()
 	{}
-	public ScoreDetail(String name,int score,Student student)
+	public ScoreDetail(String name,int score)
 	{
 		this.gameName=name;
 		this.score=score;
-		this.student=student;
+		
 	}
-	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
+
 	public String getGameName() {
 		return gameName;
 	}
@@ -44,18 +30,6 @@ public class ScoreDetail {
 		this.score = score;
 	}
 	
-	/*@ManyToOne
-	@JoinColumn(name="student_id")
-	public Student getStudent()
-	{
-		return student;
-	}
-	
-	public void setStudent(Student student)
-	{
-		this.student=student;
-	}
-	*/
 	
 	
 }
